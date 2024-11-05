24746c97bb4760df6776e51932c7a20c1c33b0f8
Bkarybaev <baielkarybaev@gmail.com>
2024-11-05 12:28:36 +0600

    cat file add

---
+++ src/Cat.java
@@ -0,0 +1,16 @@
+public class Cat {
+    String name;
+    String color;
+    int age;
+
+    public Cat(String name,int age){};
+
+    public Cat(String color){
+        this.color = color;
+    }
+
+    public Cat(int age){
+        this.age = age ;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,21 @@
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-            // test
+        Cat cat = new Cat("red");
+        Cat cat1 = new Cat(23);
+        Cat cat12 = new Cat("black");
+
+        Cat[] array = new Cat[3];
+        array[0] = cat;
+        array[1] = cat1;
+        array[2] = cat12;
+
+        for (Cat cat2 : array) {
+            System.out.println(cat2.name+"\n"+ cat2.color+"\n"+cat2.age);
         }
     }
 }
